350099306d9cf637c5b61b1dc7971490d2eeb876
Hiếu Văn Trần <hieu.tran@enouvo.com>
2019-12-10 15:29:40 +0700

    explain and upgrade cypress version

--- cypress/integration/cypress-otp.test.js
+++ cypress/integration/cypress-otp.test.js
@@ -1,16 +1,21 @@
 /// <reference types="Cypress" />
 
+
 context("Tests", () => {
   const getSecretFromPage = () =>
     cy.waitUntil(() =>
       cy
         .contains("Demo Secret")
         .then(
-          $el =>
-            $el
-              .parent()
-              .parent()
-              .find("input")[0].value
+          $el => {
+            console.log('log-aa ', $el)
+            console.log('log-bb ', $el.parent().parent())
+            return $el
+            .parent()
+            .parent()
+            .find("input")[0].value
+          }
+           
         )
         .then(value => value !== "Loading..." && value)
     );
@@ -27,15 +32,17 @@ context("Tests", () => {
     cy.visit("/");
 
     getSecretFromPage().then(secret => {
-      cy.task("generateOTP", secret).then(fillFormWithSecret);
+      cy.task("generateOTP", secret).then(val => fillFormWithSecret(val));
     });
   });
 
   it("The last OTP secret shuold be used if not passed everytime", () => {
     cy.visit("/");
     getSecretFromPage().then(secret => {
-      cy.task("generateOTP", secret);
-      cy.task("generateOTP").then(fillFormWithSecret);
+      // secret get in file src/index.js
+      cy.task("generateOTP", );
+      // generateOTP in file plugin/index.js
+      cy.task("generateOTP").then(val => fillFormWithSecret(val));
     });
   });
 });
